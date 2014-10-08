293e3c730b80b7a98f383b76fe2d27843b57d757
Spike Nox <spike01@gmail.com>
2014-10-08 14:25:52 +0100

    DID IT

--- lib/student.rb
+++ lib/student.rb
@@ -10,10 +10,13 @@ class Student
 		@awards << name 
 	end
 
+  def method_missing name
+    return true if awards.include? name.chopup 
+  end
 
-	def method_missing has_badge, badge_name
-		self.class.send(:define_method, has_badge){true}
-		# self.send(name)
-	end
+  def respond_to_missing? name, include_private = false
+    return true if name.to_s.start_with? "has" 
+    super
+  end
 
 end
